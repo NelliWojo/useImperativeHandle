461eb166d49dc6c53d6c502777669c4369bfffaf
NelliWojo <120693666+Raftouch@users.noreply.github.com>
2024-03-23 17:12:53 +0100

    feat :bento: add text input w/ ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useRef } from "react";
 import Counter, { CounterRef } from "./components/Counter";
+import TextInput, { TextInputRef } from "./components/TextInput";
 
 function App() {
   const counterRef = useRef<CounterRef>(null);
+  const inputRef = useRef<TextInputRef>(null);
 
   return (
     <>
@@ -10,8 +12,14 @@ function App() {
       <button onClick={() => counterRef.current?.reset()}>
         Reset from Parent
       </button>
-      <button onClick={() => counterRef.current?.checkSubscribed(true)}>
+
+      {/* <button onClick={() => counterRef.current?.checkSubscribed(true)}>
         Check subscription
+      </button> */}
+
+      <TextInput ref={inputRef} />
+      <button onClick={() => inputRef.current?.reset()}>
+        Reset from Parent
       </button>
     </>
   );
